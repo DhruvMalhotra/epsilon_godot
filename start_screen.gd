fd2fa9ff8bac8d1392b06d5b2ebe1c1085ff0264
Dhruv Malhotra <drv.dhruv@gmail.com>
2024-09-27 23:15:20 -0400

    added talking screen

--- start_screen.gd
+++ start_screen.gd
@@ -7,9 +7,11 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed:
-		get_tree().change_scene("res://talking_screen.tscn")
+		var talking_screen = get_tree().root.get_node("root/talking_screen")
+		self.hide()
+		talking_screen.show()
